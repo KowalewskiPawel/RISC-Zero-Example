d1e53f253765d1f3039f46519824a2ca505f43e6
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-04-29 06:34:25 +0200

    add base multiply working project boilerplate

---
+++ host/src/main.rs
@@ -0,0 +1,40 @@
+// TODO: Update the name of the method loaded by the prover. E.g., if the method
+// is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
+// `METHOD_NAME_ID` with `MULTIPLY_ID`
+use methods::{MULTIPLY_ELF, MULTIPLY_ID};
+use risc0_zkvm::Prover;
+use risc0_zkvm::{
+    serde::{from_slice, to_vec}
+};
+
+fn main() {
+    let a: u64 = 17;
+    let b: u64 = 23;
+    // Make the prover.
+    let mut prover =
+        Prover::new(MULTIPLY_ELF).expect("Prover should be constructed from valid ELF binary");
+
+    // TODO: Implement communication with the guest here
+
+    prover.add_input_u32_slice(&to_vec(&a).expect("should be serializable"));
+   prover.add_input_u32_slice(&to_vec(&b).expect("should be serializable"));
+
+    // Run prover & generate receipt
+    let receipt = prover.run().expect(
+        "Code should be provable unless it had an error or exceeded the maximum cycle limit",
+    );
+
+    // TODO: Implement code for transmitting or serializing the receipt for
+    // other parties to verify here
+
+    let c: u64 = from_slice(&receipt.journal).unwrap();
+
+    // Print an assertion
+    println!("Hello, world! I know the factors of {}, and I can prove it!", c);
+
+    // Optional: Verify receipt to confirm that recipients will also be able to
+    // verify your receipt
+    receipt.verify(&MULTIPLY_ID).expect(
+        "Code you have proven should successfully verify; did you specify the correct image ID?",
+    );
+}
