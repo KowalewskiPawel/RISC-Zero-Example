1faf5564ebcb6822698826b74d05f4e7577813a2
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-04-29 09:49:37 +0200

    add tests

--- host/src/main.rs
+++ host/src/main.rs
@@ -34,3 +34,39 @@ fn main() {
         "Code you have proven should successfully verify; did you specify the correct image ID?",
     );
 }
+
+#[cfg(test)]
+mod tests {
+    use methods::{MULTIPLY_ELF, MULTIPLY_ID};
+    use risc0_zkvm::{
+        serde::{from_slice, to_vec},
+        Prover,
+    };
+
+    const TEST_FACTOR_ONE: u64 = 17;
+    const TEST_FACTOR_TWO: u64 = 23;
+
+    #[test]
+    fn run_factors() {
+        let mut prover = Prover::new(MULTIPLY_ELF).expect(
+            "Prover should be constructed from valid method source code and corresponding method ID",
+        );
+
+        prover.add_input_u32_slice(&to_vec(&TEST_FACTOR_ONE).expect("should be serializable"));
+        prover.add_input_u32_slice(&to_vec(&TEST_FACTOR_TWO).expect("should be serializable"));
+
+        let receipt = prover.run().expect("Should be able to prove valid code");
+        receipt
+            .verify(&MULTIPLY_ID)
+            .expect("Proven code should verify");
+
+        let result: u64 = from_slice(&receipt.journal).expect(
+            "Journal output should deserialize into the same types (& order) that it was written",
+        );
+        assert_eq!(
+            result,
+            TEST_FACTOR_ONE * TEST_FACTOR_TWO,
+            "We expect the zkVM output to be the product of the inputs"
+        )
+    }
+}
