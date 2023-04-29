55f389d764e4484a9a808fa144da30643da6ecc4
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-04-29 07:02:12 +0200

    add executor

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,36 +1,74 @@
+// Copyright 2023 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use methods::{MULTIPLY_ELF, MULTIPLY_ID};
-use risc0_zkvm::serde::{from_slice, to_vec};
-use risc0_zkvm::Prover;
+use risc0_zkvm::{
+    serde::{from_slice, to_vec},
+    Executor, ExecutorEnv, SessionReceipt,
+};
 
 fn main() {
-    let a: u64 = 17;
-    let b: u64 = 23;
-    // Make the prover.
-    let mut prover =
-        Prover::new(MULTIPLY_ELF).expect("Prover should be constructed from valid ELF binary");
+    // Pick two numbers
+    let (receipt, _) = factors(17, 23);
 
-    prover.add_input_u32_slice(&to_vec(&a).expect("should be serializable"));
-    prover.add_input_u32_slice(&to_vec(&b).expect("should be serializable"));
+    // Here is where one would send 'receipt' over the network...
 
-    // Run prover & generate receipt
-    let receipt = prover.run().expect(
-        "Code should be provable unless it had an error or exceeded the maximum cycle limit",
-    );
-
-    // Code for transmitting or serializing the receipt for
-    // other parties to verify here
-
-    let c: u64 = from_slice(&receipt.journal).unwrap();
-
-    // Print an assertion
-    println!(
-        "Hello, world! I know the factors of {}, and I can prove it!",
-        c
-    );
-
-    // Optional: Verify receipt to confirm that recipients will also be able to
-    // verify your receipt
-    receipt.verify(&MULTIPLY_ID).expect(
+    // Verify receipt, panic if it's wrong
+    receipt.verify(MULTIPLY_ID).expect(
         "Code you have proven should successfully verify; did you specify the correct image ID?",
     );
+}
+
+// Multiply them inside the ZKP
+fn factors(a: u64, b: u64) -> (SessionReceipt, u64) {
+    let env = ExecutorEnv::builder()
+        // Send a & b to the guest
+        .add_input(&to_vec(&a).unwrap())
+        .add_input(&to_vec(&b).unwrap())
+        .build();
+
+    // First, we make an executor, loading the 'multiply' ELF binary.
+    let mut exec = Executor::from_elf(env, MULTIPLY_ELF).unwrap();
+
+    // Run the executor to produce a session.
+    let session = exec.run().unwrap();
+
+    // Prove the session to produce a receipt.
+    let receipt = session.prove().unwrap();
+
+    // Extract journal of receipt (i.e. output c, where c = a * b)
+    let c: u64 = from_slice(&receipt.journal).expect(
+        "Journal output should deserialize into the same types (& order) that it was written",
+    );
+
+    // Print an assertion
+    println!("I know the factors of {}, and I can prove it!", c);
+
+    (receipt, c)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn factors() {
+        const TEST_FACTOR_ONE: u64 = 17;
+        const TEST_FACTOR_TWO: u64 = 23;
+        let (_, result) = super::factors(17, 23);
+        assert_eq!(
+            result,
+            TEST_FACTOR_ONE * TEST_FACTOR_TWO,
+            "We expect the zkVM output to be the product of the inputs"
+        )
+    }
 }
